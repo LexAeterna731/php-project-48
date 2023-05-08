73a950dd8b45c6349c79c45f5cf4fc797fec0103
LexAeterna731 <Delirium731@yandex.ru>
2023-05-09 00:38:01 +0700

    add diff generator

---
+++ src/DiffGenerator.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Hexlet\Code\DiffGenerator;
+
+function isBoolean($value) {
+    $result = $value;
+
+    if (is_bool($result)) {
+        $result = $result ? "true" : "false";
+    }
+
+    return $result;
+}
+
+function compareDiff($key, $firstFile, $secondFile)
+{
+    $firstVal = isBoolean($firstFile[$key] ?? null);
+    $secondVal = isBoolean($secondFile[$key] ?? null);
+
+    if (!array_key_exists($key, $firstFile)) {
+        $comparedString = "  + {$key}: {$secondVal}";
+    } elseif (!array_key_exists($key, $secondFile)) {
+        $comparedString = "  - {$key}: {$firstVal}";
+    } elseif ($firstVal === $secondVal) {
+        $comparedString = "    {$key}: {$firstVal}";
+    } else {
+        $comparedString = "  - {$key}: {$firstVal}\n  + {$key}: {$secondVal}";
+    }
+
+    return $comparedString;
+}
+
+function generateDiff(string $firstFilePath, string $secondFilePath)
+{
+    $firstFile = json_decode(file_get_contents(realpath($firstFilePath)), true);
+    $secondFile = json_decode(file_get_contents(realpath($secondFilePath)), true);
+
+    $firstKeysArray = array_keys($firstFile);
+    $secondKeysArray = array_keys($secondFile);
+    $keysArray = array_merge($firstKeysArray, $secondKeysArray);
+    $uniqueKeys = array_unique($keysArray);
+    sort($uniqueKeys);
+
+    $resultArray = array_reduce($uniqueKeys, function ($acc, $key) use ($firstFile, $secondFile) {
+        return array_merge($acc, [compareDiff($key, $firstFile, $secondFile)]);
+    }, []);
+
+    $result = implode("\n", $resultArray);
+
+    return "{\n{$result}\n}\n";
+}
---
+++ src/HelpReader.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Hexlet\Code\HelpReader;
+
+function readHelp()
+{
+    return <<<DOC
+    Generate diff
+    
+    Usage:
+        gendiff (-h|--help)
+        gendiff (-v|--version)
+        gendiff [--format <fmt>] <firstFile> <secondFile>
+    
+    Options:
+        -h --help                     Show this screen
+        -v --version                  Show version
+        --format <fmt>                Report format [default: stylish]
+    DOC;
+}
